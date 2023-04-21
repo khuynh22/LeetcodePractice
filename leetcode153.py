8a70442973c3ac7222c374a62a22da35dcdf0888
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-04-20 22:17:50 -0500

    1 line answer

--- leetcode153.py
+++ leetcode153.py
@@ -1,14 +1,15 @@
 class Solution:
     def findMin(self, nums) -> int:
-        l, r = 0, len(nums) - 1
-        while (l < r):
-            mid = (l + r) // 2
-            if nums[mid] > nums[r]:
-                l = mid + 1
-            else:
-                r = mid
+        # l, r = 0, len(nums) - 1
+        # while (l < r):
+        #     mid = (l + r) // 2
+        #     if nums[mid] > nums[r]:
+        #         l = mid + 1
+        #     else:
+        #         r = mid
 
-        return nums[l]
+        # return nums[l]
+        return min(nums)
 
 s = Solution()
 print(s.findMin([3,4,5,1,2]))
