8840175e120aeca33cf48f34b6ee4d58ffa98106
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-04-22 10:41:58 -0500

    leetcpde 1 2sum

---
+++ leetcode1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums, target):
+        d = {}
+        for i, j in enumerate(nums):
+            r = target - j
+            if r in d: return [d[r], i]
+            d[j] = i
+
+
+print([2,7,11,15], 9)
+print([3,2,4], 6)
+print([3,3], 6)
