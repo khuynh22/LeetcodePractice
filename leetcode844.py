5780e4169e8abfec9c592ee33d0d4c9dcff3ce68
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-04-23 22:22:05 -0500

    leetcode844

---
+++ leetcode844.py
@@ -0,0 +1,42 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack1 = []
+        stack2 = []
+        p1 = 0
+        p2 = 0
+        while p1 < len(s) and p2 < len(t):
+            if s[p1] != '#':
+                stack1.append(s[p1])
+            else:
+                if len(stack1) > 0:
+                    stack1.pop()
+
+            if t[p2] != '#':
+                stack2.append(t[p2])
+            else:
+                if len(stack2) > 0:
+                    stack2.pop()
+
+            p1 += 1
+            p2 += 1
+
+        if p1 < len(s):
+            while p1 < len(s):
+                if s[p1] != '#':
+                    stack1.append(s[p1])
+                else:
+                    if len(stack1) > 0:
+                        stack1.pop()
+                p1 += 1
+
+        elif p2 < len(t):
+            while p2 < len(t):
+                if t[p2] != '#':
+                    stack2.append(t[p2])
+                else:
+                    if len(stack2) > 0:
+                        stack2.pop()
+                p2 += 1
+
+        return stack1 == stack2
+            
