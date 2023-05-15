1b7469d72b8a3b3e6cbe6256f94e56f97b27b47f
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-05-15 01:22:10 -0500

    review binary

---
+++ leetcode704.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums, target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while (l <= r):
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        return -1
+    
+s = Solution()
+print(s.search([-1,0,3,5,9,12], 9))
+print(s.search([-1,0,3,5,9,12], 2))
