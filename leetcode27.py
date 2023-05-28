4bc620d0fae9ff22c6ece2082cf3da183243ec79
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-05-28 13:43:19 -0500

    leetcode27

---
+++ leetcode27.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeElement(self, nums, val: int) -> int:
+        size = 0
+        for i in range(len(nums)):
+            if nums[i] == val:
+                size += 1
+                nums[i] = 999
+        
+        nums.sort()
+        return len(nums) - size
+        
+
+s = Solution()
+print(s.removeElement([3,2,2,3], 3))
+print(s.removeElement([0,1,2,2,3,0,4,2], 2))
