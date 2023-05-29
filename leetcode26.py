cf73b3fbf42a1a2a5330e7f6c625c9100c6c605f
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-05-28 20:55:07 -0500

    leetcode 26

---
+++ leetcode26.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums) -> int:
+        i = 1
+        for a in range(len(nums) - 1):
+            if (nums[a] != nums[a + 1]):
+                nums[i] = nums[a + 1]
+                i += 1
+
+        return i
+
+s = Solution()
+print(s.removeDuplicates([1,1,2]))
+print(s.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
