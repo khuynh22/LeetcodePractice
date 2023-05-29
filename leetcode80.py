139c687837d5f86c0cb9a7b2098a18276726c23d
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-05-28 23:39:48 -0500

    leetcode 80

---
+++ leetcode80.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeDuplicates(self, nums) -> int:
+        if len(nums) < 2:
+            return len(nums)
+    
+        count = 0 
+        slow = 0
+        fast = 2
+        while fast < len(nums):
+            if nums[slow] != nums[fast]:
+                fast += 1
+                slow = fast - 2
+            else:
+                nums[fast] = 999
+                count += 1
+                fast += 1
+
+        nums.sort()
+        return len(nums) - count
+
+    
