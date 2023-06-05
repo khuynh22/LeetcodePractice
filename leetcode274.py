8d31392f735590f9d3a6c84b18282a01fb85c853
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-05 11:49:21 -0500

    h-index

---
+++ leetcode274.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hIndex(self, citations) -> int:
+        citations.sort()
+        for i, v in enumerate(citations):
+            if len(citations) - i <= v:
+                return len(citations) - i
+        return 0
+    
+s = Solution()
+print(s.hIndex([3,0,6,1,5]))
+print(s.hIndex([1,3,1]))
