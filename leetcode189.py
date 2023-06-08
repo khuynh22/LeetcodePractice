4e5ea7004ddf4154a1b5cacdfdc1baeeae76dd03
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-08 10:42:09 -0500

    rotate array

---
+++ leetcode189.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        if k == len(nums):
+            return
+        if k > len(nums):
+            k = k - len(nums)
+
+        firstHalf = nums[:len(nums) - k]
+        secondHalf = nums[len(nums) - k:]
+        nums[:len(nums) - k] = secondHalf
+        nums[len(nums) - k:] = firstHalf
