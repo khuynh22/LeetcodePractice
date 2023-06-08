adf1b1a484f840ceac2335e893e5b2e789b191f2
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-08 01:53:07 -0500

    Candy135

---
+++ leetcode135.py
@@ -0,0 +1,17 @@
+class Solution:
+    def candy(self, ratings) -> int:
+        resArr = [1] * len(ratings)
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                resArr[i] = resArr[i - 1] + 1
+
+        for i in range(len(ratings) - 1, 0, - 1):
+            if ratings[i-1] > ratings[i]:
+                resArr[i-1] = max(resArr[i-1], resArr[i]+1)
+        return sum(resArr)
+    
+s = Solution()
+print(s.candy([1,0,2]))
+print(s.candy([1,2,2]))
+print(s.candy([1,3,2,2,1]))
+print(s.candy([1,2,87,87,87,2,1]))
