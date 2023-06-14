f68686dd97efa7a00b3d0688f52f1ea5ebb96208
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-14 12:51:30 -0500

    reverse words

---
+++ leetcode151.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split()
+        res = ""
+        for i in range(len(words) - 1, 0, -1):
+            res += words[i] + " "
+        
+        res += words[0]
+        return res
+
+s = Solution()
+print(s.reverseWords("the sky is blue"))
+print(s.reverseWords("  hello world  "))
+print(s.reverseWords("a good   example"))
