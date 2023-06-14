e547025a3f3b57b2c1844cb00075769eb4b2c206
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-14 12:25:35 -0500

    len last word 58

---
+++ leetcode58.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        res = 0
+        last = len(s) - 1
+        while s[last] == " ":
+            last -= 1
+        
+        for i in range(last, -1, -1):
+            if s[i] != " ":
+                res += 1
+            else:
+                break
+
+        return res
+    
+s = Solution()
+print(s.lengthOfLastWord("Hello World"))
+print(s.lengthOfLastWord("   fly me   to   the moon  "))
+print(s.lengthOfLastWord("luffy is still joyboy"))
