23972def895b65205fc2719ed99615f06be7e434
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-14 19:57:41 -0500

    first occurence

---
+++ leetcode28.py
@@ -0,0 +1,12 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack) - len(needle) + 1):
+            if needle == haystack[i:i + len(needle)]:
+                return i
+        
+        return -1
+    
+s = Solution()
+print(s.strStr("sadbutsad", "sad"))
+print(s.strStr("leetcode", "leeto"))
+print(s.strStr("a", "a"))
