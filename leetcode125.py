fb9c86aac4cdafb256ade44334933c323adad895
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-15 10:43:04 -0500

    valid palindrome

---
+++ leetcode125.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        convert = ""
+        for i in range(len(s)):
+            if s[i].isalnum():
+                convert += s[i].lower()
+        
+        return convert == convert[::-1]
+
+
+s = Solution()
+print(s.isPalindrome("A man, a plan, a canal: Panama"))
+print(s.isPalindrome("race a car"))
+print(s.isPalindrome(" "))
