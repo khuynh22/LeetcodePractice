115be0021668d84b2ed0ca521c7335b304012e35
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-16 19:54:26 -0500

    sliding windows classic problem

---
+++ leetcode3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) <= 1:
+            return len(s)
+        res = 0
+        l = 0
+        checkStr = ""
+        for r in range(len(s)):
+            if s[r] not in checkStr:
+                checkStr += s[r]
+                res = max(res, r - l + 1)
+            else:
+                res = max(res, r - l)
+                while (s[r] in checkStr):
+                    checkStr = checkStr[1:]
+                    l += 1
+                checkStr += s[r]
+                res = max(res, r - l)
+
+            print(checkStr)
+        return res
+
+
+
+        
