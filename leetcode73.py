e5cd6ed1fbda14d595c5363ccb30f3b534061c4c
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-18 15:53:09 -0500

    set matrix zeroes

---
+++ leetcode73.py
@@ -0,0 +1,30 @@
+class Solution:
+    def setZeroes(self, matrix) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row = len(matrix)
+        col = len(matrix[0])
+        rowZero = False
+        for r in range(row):
+            for c in range(col):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+        
+        
+        for r in range(1, row):
+            for c in range(1, col):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+        
+        if matrix[0][0] == 0:
+            for r in range(row):
+                matrix[r][0] = 0
+
+        if rowZero:
+            for c in range(col):
+                matrix[0][c] = 0
