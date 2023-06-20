6699d8db2b231678a103b0ebd954ceaac185c812
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-20 16:23:34 -0500

    isomorphic

---
+++ leetcode205.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        isoMap = {}
+        for i in range(len(s)):
+            if (s[i] in isoMap) and (isoMap[s[i]] == t[i]):
+                continue
+            elif (s[i] in isoMap) and (isoMap[s[i]] != t[i]):
+                return False
+            elif (s[i] not in isoMap) and (t[i] in isoMap.values()):
+                return False
+            else:
+                isoMap[s[i]] = t[i]
+        return True
+    
+s = Solution()
+print(s.isIsomorphic("egg", "add"))
+print(s.isIsomorphic("foo", "bar"))
+print(s.isIsomorphic("paper", "title"))
