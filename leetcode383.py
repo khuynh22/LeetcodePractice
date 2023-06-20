fbe11286202b3ed499a8f445f05412acf726d706
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-20 16:11:38 -0500

    ransome note

---
+++ leetcode383.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        banks = {}
+        notes = {}
+        for w in magazine:
+            banks[w] = 1 + banks.get(w, 0)
+
+        for w in ransomNote:
+            notes[w] = 1 + notes.get(w, 0)
+
+        for w in notes:
+            if not (w in banks.keys() and notes[w] <= banks[w]):
+                return False
+        return True
+
+s = Solution()
+print(s.canConstruct("a", "b"))
+print(s.canConstruct("aa", "ab"))
+print(s.canConstruct("aa", "aab"))
