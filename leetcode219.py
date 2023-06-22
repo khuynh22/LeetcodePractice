0805d7c1596dbb9010de25015f9d739479ba8e72
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-21 21:51:02 -0500

    Contains Duplicate II

---
+++ leetcode219.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums, k: int) -> bool:
+        numDict = {}
+        for i in range(len(nums)):
+            if nums[i] in numDict and abs(i - numDict[nums[i]] <= k):
+                return True
+            numDict[nums[i]] = i
+        return False
+    
+s = Solution()
+print(s.containsNearbyDuplicate([1,2,3,1], 3))
+print(s.containsNearbyDuplicate([1,0,1,1], 1))
+print(s.containsNearbyDuplicate([1,2,3,1,2,3], 2))
+print(s.containsNearbyDuplicate([99,99], 2))
+print(s.containsNearbyDuplicate([1,2,3,4,5,6,7,8,9,9], 3))
