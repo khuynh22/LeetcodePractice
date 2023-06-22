539b1d1b5cfa824c120429dcd1aad2b6593c5911
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-22 17:17:10 -0500

    Merge Interval

---
+++ leetcode56.py
@@ -0,0 +1,25 @@
+class Solution:
+    def merge(self, intervals): 
+        ans = []
+        intervals.sort()
+        i = 0
+        temp = intervals[i]
+        while i < len(intervals) - 1:
+            print(temp)
+            if intervals[i + 1][0] <= temp[1]:
+                if intervals[i + 1][1] <= temp[1]:
+                    i+=1
+                    continue
+                else:
+                    temp[1] = intervals[i + 1][1]
+            else:
+                ans.append(temp)
+                temp = intervals[i + 1]
+            i += 1
+
+        ans.append(temp)
+        return ans
+    
+s = Solution()
+print(s.merge([[1,3],[2,6],[8,10],[15,18]]))
+print(s.merge([[1,4],[4,5]]))
